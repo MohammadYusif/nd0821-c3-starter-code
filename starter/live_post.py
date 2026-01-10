218bba13d5d862b28ceb189c2cc0958ab91ff348
Mohammad Yusif <mosefbel@gmail.com>
2026-01-11 02:44:27 +0300

    the 5 screenshots with live_post py file

---
+++ starter/live_post.py
@@ -0,0 +1,26 @@
+import requests
+
+URL = "https://nd0821-c3-starter-code-04up.onrender.com/predict"
+
+payload = {
+    "age": 39,
+    "workclass": "State-gov",
+    "fnlgt": 77516,
+    "education": "Bachelors",
+    "education-num": 13,
+    "marital-status": "Never-married",
+    "occupation": "Adm-clerical",
+    "relationship": "Not-in-family",
+    "race": "White",
+    "sex": "Male",
+    "capital-gain": 2174,
+    "capital-loss": 0,
+    "hours-per-week": 40,
+    "native-country": "United-States",
+}
+
+response = requests.post(URL, json=payload)
+
+
+print("Status code:", response.status_code)
+print("Response JSON:", response.json())
